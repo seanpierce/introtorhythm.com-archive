a4c6fb91763ecbff2aa85f52a393d5051c0a8bff
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-12-08 17:20:57 -0700

    wire-up info modal

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function openModal(id) {
+	var modal = document.getElementById(id);
+	modal.style.display = 'inherit';
+}
+
+function closeModal(id) {
+	var modal = document.getElementById(id);
+	modal.style.display = 'none';
+}
