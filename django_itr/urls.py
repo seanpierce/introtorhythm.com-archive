8e1c7f483f322185841df3ba1e20273dcc7bb651
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-12-16 08:55:56 -0700

    Update urls

--- django_itr/urls.py
+++ django_itr/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -23,8 +23,8 @@ from . import views
 urlpatterns = [
 	url(r'^ep/', include('episodes.urls')),
 	url(r'', include('episodes.urls')),
-	path('api/', include('api.urls')),
-    path('admin/', admin.site.urls),
+	url(r'^api/', include('api.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
