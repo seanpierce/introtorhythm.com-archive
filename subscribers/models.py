d46aa67ae672e59dddb65fa46d6d527d26d50e63
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-12-17 16:25:13 -0700

    Format models

--- subscribers/models.py
+++ subscribers/models.py
@@ -3,24 +3,27 @@ import uuid
 from django.db import models
 
 # Create your models here.
+
+
 class Subscriber(models.Model):
-	created_at = models.DateTimeField(auto_now=True)
-	email = models.EmailField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=255)
 
-	class Meta:
-		ordering = ['email',]
+    class Meta:
+        ordering = ['email', ]
 
-	def __str__(self):
-		return self.email
+    def __str__(self):
+        return self.email
 
 
 class SubscriptionRequest(models.Model):
-	created_at = models.DateTimeField(auto_now=True)
-	email = models.EmailField(max_length=255)
-	token = uuid.uuid4()
+    created_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=255)
+    token = models.CharField(max_length=100, blank=True,
+                             unique=True, default=uuid.uuid4)
 
-	class Meta:
-		ordering = ['email',]
+    class Meta:
+        ordering = ['email', ]
 
-	def __str__(self):
-		return self.email
+    def __str__(self):
+        return self.email
