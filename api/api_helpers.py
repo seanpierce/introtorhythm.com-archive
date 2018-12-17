7023b8a0ac2b6770d656ed7ec54287360ae327da
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-12-17 15:32:30 -0700

    Add API helpers

---
+++ api/api_helpers.py
@@ -0,0 +1,32 @@
+import json
+import smtplib
+
+from django.http import HttpResponse
+
+
+class APIResponse():
+    def __init__(self, data):
+        self.data = data
+
+
+def send_confirmation_email(email):
+    sender = 'noreply@introtorhythm.com'
+    receivers = [email]
+
+    message = f"""From: Intro To Rhythm <noreply@introtorhythm.com>
+        To: Subscriber <{email}>
+        Subject: SMTP e-mail test
+
+        This is a test e-mail message. Ah yee.
+        """
+
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(sender, receivers, message)
+        return True
+    except SMTPException:
+        return False
+
+
+def response(data):
+    return HttpResponse(json.dumps(APIResponse(data).__dict__), content_type='application/json')
