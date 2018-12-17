7b58112e0e49dd572d58201128ed547c5a07b871
Sean Pierce Sumler <sumler.sean@gmail.com>
2018-12-17 14:40:24 -0700

    Added new subscriber endpoint and API helper class

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import get_episodes
+from .views import get_episodes, create_new_subscription_request
 
 
 urlpatterns = [
-    path('episodes/', get_episodes, name="episodes-all")
+    path('episodes/', get_episodes, name="episodes-all"),
+    path('subscribe/', create_new_subscription_request,
+         name="new_subscription_request"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,31 @@
+import json
+
 from django.apps import apps
 from django.core import serializers
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 Episode = apps.get_model('episodes', 'Episode')
 
+
+class APIResponse():
+    def __init__(self, data):
+        self.data = data
+
+
+def response(data):
+    return HttpResponse(json.dumps(APIResponse(data).__dict__), content_type='application/json')
+
+
 def get_episodes(request):
-    """
-    Provides a get method handler.
-    """
     episodes = Episode.objects.all()
     response = serializers.serialize("json", episodes)
     return HttpResponse(response, content_type='application/json')
+
+
+@csrf_exempt
+def create_new_subscription_request(request):
+    if request.method == 'POST':
+        return response(True)
+    else:
+        return response(False)
