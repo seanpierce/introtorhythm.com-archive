abfec38448e32cc290778a28e53558e79370f824
Sean Pierce Sumler <sumler.sean@gmail.com>
2019-01-30 15:08:42 -0700

    Paginate subscribers and subscription requests

--- subscribers/admin.py
+++ subscribers/admin.py
@@ -8,6 +8,7 @@ from .models import Subscriber, SubscriptionRequest
 
 class SubscriberAdmin(admin.ModelAdmin):
     list_display = ('email', 'created_at')
+    list_per_page = 10
     actions = ["export_as_csv"]
 
     def export_as_csv(self, request, queryset):
@@ -28,6 +29,7 @@ class SubscriberAdmin(admin.ModelAdmin):
 
 class SubscriptionRequestAdmin(admin.ModelAdmin):
     list_display = ('token', 'email', 'created_at')
+    list_per_page = 10
     actions = ["add_to_subscribers"]
 
     def add_to_subscribers(self, request, queryset):
